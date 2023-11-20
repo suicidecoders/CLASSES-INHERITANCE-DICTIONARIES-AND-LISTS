98e07b71c7e5aba0571b9813ba06af24b3fc6c75
suicide coders <149674757+suicidecoders@users.noreply.github.com>
2023-11-20 21:32:52 +0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,11 @@
+
+# TODO: write code. number 3..
+class Person:
+    def _init_(self, name, last_name):
+        self.name = name 
+        self.last_name = last_name
+            
+individual = Person
+individual.last_name = "kawooya"
+individual.name = "sudais"
+print(individual.name, individual.last_name)
---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+
+people_in_bcs = {
+    "robert": "short",
+    "sudais": "brown",
+    "ochom": "big",
+}
+
+print(people_in_bcs)
+people_in_bcs["angel"] = "bright girl"
+print(people_in_bcs)
+print(people_in_bcs[""])
